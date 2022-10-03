d24458992e56c14ecdbf2e4136fe60ebf5e5d1b7
davidpanggabean16 <davidprecius03@gmail.com>
2022-10-03 22:15:36 +0700

    first commit

---
+++ matriks 2x2.py
@@ -0,0 +1,10 @@
+#A+B
+matriksA = 3,2,3,2
+matriksB = 3,2,3,2
+
+a11 = matriksA[0] + matriksB[0]
+a12 = matriksA[1] + matriksB[1]
+a21 = matriksA[2] + matriksB[2]
+a22 = matriksA[3] + matriksB[3]
+
+print("[",a11,a12,"] \n" "[",a21,a22,"]")
